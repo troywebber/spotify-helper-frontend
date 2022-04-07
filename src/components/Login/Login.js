84e69b185fb1978f43b8ca3a2ecf21d7c8b76318
troy <troywebb95@gmail.com>
2022-04-07 14:15:58 +0100

    added env to auth url

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,7 @@
 import scss from "./login.module.scss";
 import { BsSpotify } from "react-icons/bs";
 
-const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=54dece4e1c44405d8832f941bacc28a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=54dece4e1c44405d8832f941bacc28a0&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 function Login() {
   return (
