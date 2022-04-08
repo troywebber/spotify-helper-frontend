aca8f11cce2cae66ac933cbc849e228e59bb03a9
troy <troywebb95@gmail.com>
2022-04-08 18:10:54 +0100

    returned mapped users tracks on main component and styling adjustments

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -2,5 +2,50 @@ import { atom } from "recoil";
 
 export const currentPlaylist = atom({
   key: "currentPlaylist",
-  default: "hello",
+  default: {
+    collaborative: false,
+    description: "",
+    external_urls: {
+      spotify: "https://open.spotify.com/playlist/1N1aVEPnESlZI6nlqTADkr",
+    },
+    href: "https://api.spotify.com/v1/playlists/1N1aVEPnESlZI6nlqTADkr",
+    id: "1N1aVEPnESlZI6nlqTADkr",
+    images: [
+      {
+        height: 640,
+        url: "https://mosaic.scdn.co/640/ab67616d0000b2731501f2fb9d232a41a43bfa70ab67616d0000b27348386de73fd3c90e46ad2447ab67616d0000b273540a283c2b9bf51879d0952eab67616d0000b273a386de0c91a4607c4251ac44",
+        width: 640,
+      },
+      {
+        height: 300,
+        url: "https://mosaic.scdn.co/300/ab67616d0000b2731501f2fb9d232a41a43bfa70ab67616d0000b27348386de73fd3c90e46ad2447ab67616d0000b273540a283c2b9bf51879d0952eab67616d0000b273a386de0c91a4607c4251ac44",
+        width: 300,
+      },
+      {
+        height: 60,
+        url: "https://mosaic.scdn.co/60/ab67616d0000b2731501f2fb9d232a41a43bfa70ab67616d0000b27348386de73fd3c90e46ad2447ab67616d0000b273540a283c2b9bf51879d0952eab67616d0000b273a386de0c91a4607c4251ac44",
+        width: 60,
+      },
+    ],
+    name: "TECH",
+    owner: {
+      display_name: "Troy",
+      external_urls: {
+        spotify: "https://open.spotify.com/user/113647088",
+      },
+      href: "https://api.spotify.com/v1/users/113647088",
+      id: "113647088",
+      type: "user",
+      uri: "spotify:user:113647088",
+    },
+    primary_color: null,
+    public: true,
+    snapshot_id: "MTYsMWI0ZDY0NzQzMGFjOTkxNmE3MDM5MTc1OTNkM2RkODNhZGE2NTljMw==",
+    tracks: {
+      href: "https://api.spotify.com/v1/playlists/1N1aVEPnESlZI6nlqTADkr/tracks",
+      total: 6,
+    },
+    type: "playlist",
+    uri: "spotify:playlist:1N1aVEPnESlZI6nlqTADkr",
+  },
 });
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,10 +9,12 @@ const spotifyApi = new SpotifyWebApi();
 
 export default function Main({ accessToken }) {
   const playlist = useRecoilValue(currentPlaylist);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
   const [gradient, setGradient] = useState(
     "linear-gradient(360deg, rgb(18, 18, 18) 35%, rgb(16, 140, 7))"
   );
 
+  //random color
   useEffect(() => {
     const randomColor = () => {
       const r = Math.floor(Math.random() * 256);
@@ -25,12 +27,25 @@ export default function Main({ accessToken }) {
     randomColor();
   }, []);
 
+  // set spotify access token
   useEffect(() => {
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
 
+  useEffect(() => {
+    const getPlaylistTracks = async () => {
+      setTimeout(() => {
+        spotifyApi.getPlaylistTracks(playlist.id).then((data) => {
+          setPlaylistTracks(data.body.items);
+        });
+      }, 1000);
+    };
+    getPlaylistTracks();
+  }, [playlist]);
+
+  console.log(playlistTracks);
+
   if (typeof playlist != "string") {
-    console.log(playlist);
     return (
       <div className={scss.mainContainer} style={{ background: gradient }}>
         <div className={scss.topBanner}>
@@ -42,15 +57,30 @@ export default function Main({ accessToken }) {
               height={220}
             />
           </div>
-          <div className={scss.playlistInfo}>
-            <p>playlist</p>
-            <h1 className={scss.playlistName}>{playlist.name}</h1>
-            <div className={scss.playlistOwner}>
-              <p>{playlist.owner.display_name}</p>
-              <p>likes</p>
-              <p>song total</p>
-            </div>
-          </div>
+          <h1 className={scss.playlistName}>{playlist.name}</h1>
+        </div>
+        <div className={scss.playlistTrackTitles}>
+          <p>ALBUM</p>
+          <p>TRACK</p>
+          <p>ARTIST</p>
+        </div>
+        <div className={scss.playlistTracks}>
+          {playlistTracks.map((track) => {
+            console.log(track);
+            return (
+              <div className={scss.track} key={track.track.name}>
+                <Image
+                  src={track.track.album.images[0].url}
+                  alt="trackImage"
+                  width={70}
+                  height={70}
+                />
+
+                <p>{track.track.name}</p>
+                <p>{track.track.artists[0].name}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
