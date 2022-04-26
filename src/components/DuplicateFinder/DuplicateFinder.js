7831cc512b4f2f712a993c2949770a5ab36e6fb5
troy <troywebb95@gmail.com>
2022-04-26 15:36:41 +0100

    removed spotify 100 song fetch limit, displayed selected playlist fetched data for the duplicate finder feature

--- src/components/DuplicateFinder/DuplicateFinder.js
+++ src/components/DuplicateFinder/DuplicateFinder.js
@@ -7,6 +7,9 @@ const spotifyApi = new SpotifyWebApi();
 
 function DuplicateFinder({ accessToken }) {
   const [playlists, setPlaylists] = useState([]);
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+  const [songs, setSongs] = useState([]);
+
   // set spotify access token
   useEffect(() => {
     spotifyApi.setAccessToken(accessToken);
@@ -22,40 +25,98 @@ function DuplicateFinder({ accessToken }) {
 
   //handlebutton click console log playlist id
   const handlePlaylistClick = (playlist) => {
-    console.log(playlist);
+    setSelectedPlaylist(playlist.id);
   };
 
-  return (
-    <div className={scss.mainContainer} style={{ background: "#121212" }}>
-      <h2 className={scss.playlistTitle}>DUPLICATE REMOVER COMING SOON!</h2>
-      <div className={scss.playlists}>
-        {playlists.map((playlist, index) => {
-          return (
-            <div className={scss.playlist} key={index}>
-              <div className={scss.playlistImage}>
-                <Image
-                  src={playlist.images[0].url}
-                  alt="playlistImage"
-                  width={190}
-                  height={190}
-                  style={{ borderRadius: "7px" }}
-                />
-              </div>
-              <div className={scss.playlistNameButton}>
-                <p className={scss.playlistName}>{playlist.name}</p>
-                <button
-                  className={scss.playlistButton}
-                  onClick={(e) => handlePlaylistClick(playlist)}
-                >
-                  Find Duplicates
-                </button>
-              </div>
-            </div>
+  //fetch songs from selected playlist without the 100 song limit
+  useEffect(() => {
+    if (selectedPlaylist !== null) {
+      const getPlaylistTracks = async (selectedPlaylist) => {
+        let tracks = [];
+        let offset = 0;
+        let limit = 100;
+        let total = 0;
+        let hasMore = true;
+        while (hasMore) {
+          const response = await spotifyApi.getPlaylistTracks(
+            selectedPlaylist,
+            {
+              offset,
+              limit,
+            }
           );
-        })}
-      </div>
-    </div>
-  );
-}
+          tracks = tracks.concat(response.body.items);
+          offset += limit;
+          total = response.body.total;
+          hasMore = offset < total;
+        }
+        setSongs(tracks.filter((song) => song.track !== null));
+      };
+      getPlaylistTracks(selectedPlaylist);
+    }
+  }, [selectedPlaylist]);
 
+  if (selectedPlaylist && songs.length > 0) {
+    return (
+      <div>
+        <button onClick={() => setSelectedPlaylist(null)}>Back</button>
+        <div className={scss.playlistTrackTitles}>
+          <p>ALBUM</p>
+          <p>TRACK</p>
+          <p>ARTIST</p>
+        </div>
+        <div className={scss.playlistTrackBorder}></div>
+        <div className={scss.playlistTracks}>
+          {songs.map((track, index) => {
+            console.log(track);
+            return (
+              <div className={scss.track} key={index}>
+                <Image
+                  src={track.track.album.images[0].url || ""}
+                  alt="trackImage"
+                  width={100}
+                  height={100}
+                />
+                <p>{track.track.name}</p>
+                <p>{track.track.artists[0].name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={scss.mainContainer} style={{ background: "#121212" }}>
+        <h2 className={scss.playlistTitle}>DUPLICATE REMOVER COMING SOON!</h2>
+        <div className={scss.playlists}>
+          {playlists.map((playlist, index) => {
+            return (
+              <div className={scss.playlist} key={index}>
+                <div className={scss.playlistImage}>
+                  <Image
+                    src={playlist.images[0].url}
+                    alt="playlistImage"
+                    width={190}
+                    height={190}
+                    style={{ borderRadius: "7px" }}
+                  />
+                </div>
+                <div className={scss.playlistNameButton}>
+                  <p className={scss.playlistName}>{playlist.name}</p>
+                  <button
+                    className={scss.playlistButton}
+                    onClick={(e) => handlePlaylistClick(playlist)}
+                  >
+                    Find Duplicates
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 export default DuplicateFinder;
